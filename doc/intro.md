92da3df43d654f114118f45cf28e5b8553f691c3
Theo Pack <tf.pack@gmail.com>
2013-01-12 20:15:03 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dota2stats
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/dota2stats/core_test.clj
@@ -0,0 +1,7 @@
+(ns dota2stats.core-test
+  (:use clojure.test
+        dota2stats.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
