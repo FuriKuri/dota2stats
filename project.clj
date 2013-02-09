112501d4e543afc1ac45c93d12a7061365bd2868
Theo Pack <tf.pack@gmail.com>
2013-02-09 16:21:36 +0100

    working hello world with compojure

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+  				 [compojure "1.1.5"]
+  				 ]
+  :plugins [[lein-swank "1.4.4"]
+  			[lein-ring "0.7.1"]]
+  :min-lein-version "2.0.0"
+  :ring {:handler dota2stats.core/app})
--- src/dota2stats/core.clj
+++ src/dota2stats/core.clj
@@ -1,6 +1,16 @@
-(ns dota2stats.core)
+(ns dota2stats.core
+  (:use dota2stats.views
+  	    compojure.core
+        [hiccup.middleware :only (wrap-base-url)])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes main-routes
+  (GET "/" [] (index-page))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+  (-> (handler/site main-routes)
+      (wrap-base-url)))
