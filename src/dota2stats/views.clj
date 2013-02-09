764e43584a09e713fd26f5679303f61d0daaf094
Theo Pack <tf.pack@gmail.com>
2013-02-09 17:11:43 +0100

    create template page

--- src/dota2stats/views.clj
+++ src/dota2stats/views.clj
@@ -5,6 +5,25 @@
   (html5
     [:head
       [:title "Hello World"]
-      (include-css "/css/style.css")]
+      (include-css "/css/bootstrap.css")
+      [:style "body {padding-top: 60px;}"]
+      ]
     [:body
-      [:h1 "Hello World"]]))
+      [:div {:class "navbar navbar-inverse navbar-fixed-top"}
+        [:div {:class "navbar-inner"}
+          [:div {:class "container"}
+            [:button {:type "button" :class "btn btn-navbar" :data-toggle "collapse" :data-target ".nav-collapse"}
+              [:span {:class "icon-bar"}]
+              [:span {:class "icon-bar"}]
+              [:span {:class "icon-bar"}]]
+            [:a {:href "#" :class "brand"} "Dota 2 Stats"]
+            [:div {:class "nav-collapse collapse"}
+              [:ul {:class "nav"}
+                [:li {:class "active"} [:a {:href "#"} "Home"]]
+                [:li [:a {:href "#about"} "About"]]
+                [:li [:a {:href "#"} "Contact"]]]]]]]
+      [:div {:class "container"}
+        [:h1 "Dota 2 Stats"]
+        [:P "Content"]
+      ]
+      (include-js "/js/bootstrap.js")]))
